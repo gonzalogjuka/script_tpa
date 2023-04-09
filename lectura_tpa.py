8059d2601314c3448231b4433f4bc82ee71741fa
Gonzalo Gjuka <gonzalo.gjuka@hotmail.com>
2023-04-10 01:52:34 +0200

    ok

--- lectura_tpa.py
+++ lectura_tpa.py
@@ -8,32 +8,37 @@ while True:
     print('0: Salir')
     opcion = input('Ingrese una opción: ')
 
-
-
     if opcion == '1':
         pos_number = input('Ingrese número de pos: ')
         url_way = r"D:\newpos61\POSFILES\LOGS\tlog\POS00" + pos_number
+        tpa_way = url_way + ""
         print('El número de pos es el siguiente: ' + url_way)
 
-    try:
-        with open(url_way, 'r') as archivo:
-            contenido = archivo.read()
-            if '997110761301310013' in contenido:
-                print('Se encontró cierre de TPA.')
-            else:
-                print('No se encontró encontró cierre de TPA.')
-    except FileNotFoundError:
-        print('El archivo no pudo ser encontrado. Verifica la ruta y el número de pos ingresados.')
+        try:
+            with open(url_way, 'r') as archivo:
+                contenido = archivo.read()
+                if '997110761301310013' in contenido:
+                    cierre_tpa = "Ok"
+                    print('Se encontró cierre de TPA.')
+                else:
+                    cierre_tpa = "Fail"
+                    print('No se encontró encontró cierre de TPA.')
+        except FileNotFoundError:
+            print('El archivo no pudo ser encontrado. Verifica la ruta y el número de pos ingresados.')
 
-        elif opcion == '2':        
+    elif opcion == '2':
         ip_pos = input('Ingrese IP completa de pos: ')
         url_pos = "\\" + ip_pos + r"\e$\newpos61\posfile\logs"
         print('La IP elegida es: ' + ip_pos)
-        elif opcion == '0':
+
+    elif opcion == '0':
         print('Saliendo...')
         break
+
     else:
         print('Opción no válida. Por favor, ingrese una opción válida.')
+
+
 
 # una vez que toma la info correcta analizar tpa y anotar evt contarlos y mostrarlos (dar una preview de los eventos)
 # hacer seccion de resultados de los test (si tiene o no el resultado obtenido) y poner ok o fail
